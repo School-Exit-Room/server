c1b4d63f2e88b2ca1c86b6d94c32c381f86abb2d
Marian Hahne <marian.hahne@outlook.com>
2021-04-18 21:03:23 +0200

    Add flaschenpost for logging

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import { flaschenpost, getMiddleware } from 'flaschenpost';
 
 // Rest of the code remains same
 const app = express();
 const port = '8000';
 
+const logger = flaschenpost.getLogger();
+
+// Add Express middleware
+app.use(getMiddleware({ logOn: 'request' }));
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  logger.info(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
