2d919320473e62aeb4f926e7d3cd392f2da06bc6
Marian Hahne <marian.hahne@outlook.com>
2021-04-21 17:28:50 +0200

    Add and config GraphQL endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
+import { buildSchema } from 'graphql';
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+
 import { flaschenpost, getMiddleware } from 'flaschenpost';
 
-// Rest of the code remains same
+const logger = flaschenpost.getLogger();
+
+// --------------
+// Express config
+// --------------
 const app = express();
 const port = '8000';
-
-const logger = flaschenpost.getLogger();
 
 // Add Express middleware
 app.use(getMiddleware({ logOn: 'request' }));
@@ -14,6 +19,32 @@ app.get('/', (request, response): any => {
   response.send('Express + TypeScript Server');
 });
 
+// --------------
+// GraphQL config
+// --------------
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello (): string {
+    return 'Hello world!';
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+// Start server
 app.listen(port, (): void => {
   logger.info(`⚡️[server]: Server is running at http://localhost:${port}`);
+  logger.info(`⚡️[server]: GraphQL endpoint is running at http://localhost:${port}/graphql`);
 });
